f3b7ab781d8bad417ccbf150a5ccb69f57994c01
gmatolo <mulimatolo@gmail.com>
2023-04-01 07:51:34 +0000

    Adds the-get-user-email component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { NotFoundComponent } from './shared/not-found/not-found.component';
 import { HomeComponent } from './components/home/home.component';
+import { UserEmailComponent } from './components/user-email/user-email.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { HomeComponent } from './components/home/home.component';
     FooterComponent,
     HeaderComponent,
     NotFoundComponent,
-    HomeComponent
+    HomeComponent,
+    UserEmailComponent
   ],
   imports: [
     BrowserModule,
