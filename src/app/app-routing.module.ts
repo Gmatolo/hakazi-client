bdbd2d88b328267001e543c64825912396d95cb1
gmatolo <mulimatolo@gmail.com>
2023-04-01 17:12:17 +0000

    Updates the user-email component and adds it angular routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { UserEmailComponent } from './components/user-email/user-email.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'user-email', component: UserEmailComponent },
   { path: '**', redirectTo: '' },
 ];
 
