3622ff87f18fe9a07b898aa321749c15ed877f34
gmatolo <mulimatolo@gmail.com>
2023-04-01 18:27:27 +0000

    Updates user-email

--- src/app/components/user-email/user-email.component.ts
+++ src/app/components/user-email/user-email.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-user-email',
   templateUrl: './user-email.component.html',
   styleUrls: ['./user-email.component.scss']
 })
-export class UserEmailComponent implements OnInit {
+export class UserEmailComponent {
 
-  constructor() { }
+  email: string = "";
+  errorMessage: string = "";
 
-  ngOnInit(): void {
+  constructor(private http: HttpClient, private router: Router) { }
+
+  onSubmit(): void {
+    this.http.post('http://localhost:8000/api/get_user_email/', { email: this.email })
+      .pipe(
+        tap((response: any) => {
+          if (!response.success) {
+            throw new Error(response.errors.email[0]);
+          }
+        }),
+        catchError((error: any) => {
+          this.errorMessage = error.message;
+          return throwError(() => new Error(error))
+        })
+      )
+      .subscribe(() => {
+        this.router.navigate(['/resume']);
+      });
   }
 
 }
