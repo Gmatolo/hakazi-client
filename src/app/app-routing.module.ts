80e6d6f01e5aad6cc47d9842b5ad9070d5106508
gmatolo <mulimatolo@gmail.com>
2023-04-01 18:26:49 +0000

    Adds angular routing for resume-upload and resume-upload-success components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { ResumeUploadSuccessComponent } from './components/resume-upload-success/resume-upload-success.component';
+import { ResumeUploadComponent } from './components/resume-upload/resume-upload.component';
 import { UserEmailComponent } from './components/user-email/user-email.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'user-email', component: UserEmailComponent },
+  { path: 'submit-user-email', component: UserEmailComponent },
+  { path: 'resume', component: ResumeUploadComponent },
+  { path: 'upload-success', component: ResumeUploadSuccessComponent },
   { path: '**', redirectTo: '' },
 ];
 
